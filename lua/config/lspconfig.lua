d04b932e02f9050872e980afba1148d944247165
zinguru <zinguru@gmail.com>
2025-01-06 16:09:34 +0300

    added javascript lsp

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -170,8 +170,8 @@ return {
       -- Some languages (like typescript) have entire language plugins that can be useful:
       --    https://github.com/pmizio/typescript-tools.nvim
       --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
+      -- Butsservert for many setups, the LSP (`ts_ls`) will work just fine
+      ts_ls = {},
       --
 
       lua_ls = {
